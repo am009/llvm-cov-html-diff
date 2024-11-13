daa82a4af18968bc8d1894395f02b782faff57a2
am009 <warrenwjk@qq.com>
2024-11-13 06:03:50 +0000

    Update to package.

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # llvm-cov-html-diff
 
-Thanks to https://github.com/Dentosal/llvm-codecov-diff/blob/0352d07e486f75b2dfb8bf2d7eb8a6e948e19106/app.py
-
 This tool allows you to inspect and debug fuzzbench results to know why one fuzzer is better than the other.
 - Enable the generation of llvm-cov coverage report.
 - Use this tool to diff the report.
@@ -16,12 +14,19 @@ This tool allows you to inspect and debug fuzzbench results to know why one fuzz
 python full_report_diff.py <old_report_folder> <new_report_folder> <output_report_folder>
 ```
 
+## Develop
+
+```
+python3 -m build && pip install ./dist/llvmcovdiff-0.0.1.tar.gz
+```
+
+## Details
+
 In generated report from llvm-cov
 - There are `index.html` that shows the main status of the coverage.
 - There are `xxxx.c.html` or `xxx.h.html` that shows line coverage.
 
 Given two llvm-cov report folder, We generate a new folder that shows the differences.
-
 
 ## FAQ
 
@@ -30,6 +35,8 @@ Given two llvm-cov report folder, We generate a new folder that shows the differ
 For line coverage count, we only highlight those counts that goes from zero to non-zero, or from non-zero to zero.
 
 ## Others
+
+Thanks to https://github.com/Dentosal/llvm-codecov-diff/blob/0352d07e486f75b2dfb8bf2d7eb8a6e948e19106/app.py
 
 Tested on python 3.10 pip 24.2:
 
--- src/llvmcovdiff/full_report_diff.py
+++ src/llvmcovdiff/full_report_diff.py
@@ -1,43 +1,48 @@
 import os
-import argparse
+from .diff_line_coverage import diff_line_coverage
+from .diff_main_report import diff_main_report
 
-script_path = os.path.realpath(__file__)
-script_dir = os.path.dirname(script_path)
-parent_dir = os.path.dirname(script_dir)
+def diff_full_report(old_path, new_path, out_path):
 
-parser = argparse.ArgumentParser(
-    prog="llvm-codecov-diff",
-    description="A html diff generator for llvm html coverage reports",
-)
-# old report folder
-parser.add_argument("old")
-# new report folder
-parser.add_argument("new")
-parser.add_argument("out", nargs="?")
-args = parser.parse_args()
+    join_index = lambda x: os.path.join(x, "index.html")
+    try:
+        print(f"Processing main report.")
+        diff_main_report(join_index(old_path), join_index(new_path), join_index(out_path))
+    except:
+        print(f"Error occurred, to debug or reproduce, run:")
+        print(f"python3 -m llvmcovdiff.diff_main_report {join_index(old_path)} {join_index(new_path)} {join_index(out_path)}\n")
 
-def diff_main_report(old, new, out):
-    os.makedirs(os.path.dirname(out), exist_ok=True)
-    return os.system(f'python3 {script_dir}/diff_main_report.py {old} {new} {out}')
-
-def diff_each_file(old, new, out):
-    os.makedirs(os.path.dirname(out), exist_ok=True)
-    return os.system(f'python3 {script_dir}/diff_each_file.py {old} {new} {out}')
-
-join_index = lambda x: os.path.join(x, "index.html")
-diff_main_report(join_index(args.old), join_index(args.new), join_index(args.out))
-
-for root, dirs, files in os.walk(args.old, topdown=False):
-   for name in files:
-        if name.endswith('index.html'):
-           continue
-        if name.endswith('.html'):
-            old_file = os.path.join(root, name)
-            rel_path = os.path.relpath(old_file, args.old)
-            new_file = os.path.join(args.new, rel_path)
-            result_file = os.path.join(args.out, rel_path)
-            if os.path.exists(result_file):
+    for root, dirs, files in os.walk(old_path, topdown=False):
+        for name in files:
+            if name.endswith('index.html'):
                 continue
-            result = diff_each_file(old_file, new_file, result_file)
-            if result != 0:
-                print(f"python3 {script_dir}/diff_each_file.py {old_file} {new_file} {result_file}")
+            if name.endswith('.html'):
+                old_file = os.path.join(root, name)
+                rel_path = os.path.relpath(old_file, old_path)
+                new_file = os.path.join(new_path, rel_path)
+                result_file = os.path.join(out_path, rel_path)
+                if os.path.exists(result_file):
+                    continue
+                try:
+                    print(f"Processing: {rel_path}", file=sys.stderr)
+                    diff_line_coverage(old_file, new_file, result_file)
+                except:
+                    print(f"Error occurred, to debug or reproduce, run:")
+                    print(f"python3 -m llvmcovdiff.diff_line_coverage {old_file} {new_file} {result_file}\n")
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(
+        prog="llvmcovdiff",
+        description="A html diff generator for llvm html coverage reports",
+    )
+    # old report folder
+    parser.add_argument("old")
+    # new report folder
+    parser.add_argument("new")
+    parser.add_argument("out")
+    args = parser.parse_args()
+    diff_full_report(args.old, args.new, args.out)
+
+if __name__ == '__main__':
+    main()
